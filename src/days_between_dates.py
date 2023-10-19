97ca46bee905fa5301f1a77f5ad6ba265dc9331a
Aarón Silvoso García <148151768+AlakX86@users.noreply.github.com>
2023-10-19 19:51:31 +0200

    Update days_between_dates.py
    Update of the main code to adapt to the update of next_month

--- src/days_between_dates.py
+++ src/days_between_dates.py
@@ -10,18 +10,19 @@ def days_between_dates():
         raise ValueError("La fecha inicial debe ser anterior o igual a la fecha final")
 
     day, month, year = 0, 0, 0
-    while [start_year, start_month, start_day] != [final_year, final_month, final_day]:
-        start_year, start_month, start_day = next_day(start_year, start_month, start_day)
+    start_year_copy, start_month_copy, start_day_copy = start_year, start_month, start_day
+    while [start_year_copy, start_month_copy, start_day_copy] != [final_year, final_month, final_day]:
+        start_year_copy, start_month_copy, start_day_copy = next_day(start_year_copy, start_month_copy, start_day_copy)
 
-        if next_month(start_month, start_day):
-            month += 1
-
-        day += 1
-        if next_year(start_month,start_day):
+        if next_year(start_month_copy,start_day_copy):
             year += 1
-
+        
+        if next_month(start_day_copy, start_day,start_year_copy,start_month_copy):
+            month += 1
+        
+        day += 1
+        
     return f"total days:{day} \ntotal months:{month} \ntotal years:{year} \nIn clear terms: {year} years and {month%12} months"
-
 
 print(days_between_dates())
 input("Presiona Enter para salir...")
