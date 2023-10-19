b7deab55e5dfd7ddc3119ab237a636df7bfccaf9
Aarón Silvoso García <148151768+AlakX86@users.noreply.github.com>
2023-10-19 19:50:31 +0200

    Update next_month.py
    Update to meet more test cases, as before it could give 1 month or 1 month too much depending on the start/end day. Now this should not happen and should be accurate in all cases.

--- src/next_month.py
+++ src/next_month.py
@@ -1,5 +1,15 @@
-def next_month(month,day):
-    if month == 2:
-        return day == 28
-    elif day == 29:
-        return day
+from is_leap_year import is_leap_year
+def next_month(day, first_day,year,month):
+    if day == first_day:
+        return True
+    if first_day in [29,30,31]:
+        if month == 2:
+            if is_leap_year(year):
+                if day == 29:
+                    return True
+            if not is_leap_year(year):
+                if day == 28:
+                    return True
+        if month in [1,3,4,5,6,7,8,9,10,11,12]:
+            if day == first_day:
+                return True
