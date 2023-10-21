24d9dfc5f0e35ca8ade85cbb58c459166efd9e62
Aarón Silvoso Garcia <93611856+alakinepepine@users.noreply.github.com>
2023-10-21 10:42:33 +0200

    Fix next_year
    The function next_year has been corrected to correctly give the year since it gave 1 extra year only if it passed the year, from December 31, 2020 to January 1, 2021 it gave 1 year. Now it should work properly, and 2 test cases have been added

--- src/days_between_dates.py
+++ src/days_between_dates.py
@@ -12,7 +12,7 @@ def days_between_dates(start_year, start_month, start_day, final_year, final_mon
     while [start_year_copy, start_month_copy, start_day_copy] != [final_year, final_month, final_day]:
         start_year_copy, start_month_copy, start_day_copy = next_day(start_year_copy, start_month_copy, start_day_copy)
 
-        if next_year(start_month_copy,start_day_copy):
+        if next_year(start_month_copy, start_day_copy, start_month, start_day, start_year_copy):
             year += 1
         
         if next_month(start_day_copy, start_day,start_year_copy,start_month_copy):
@@ -20,9 +20,12 @@ def days_between_dates(start_year, start_month, start_day, final_year, final_mon
         
         day += 1
         
-    return day, month, year
+    return year,month,day
 
 if __name__ == "__main__":
-    assert days_between_dates(2000, 2, 29, 2023, 3, 1) == (8401, 276, 23)
-    assert days_between_dates(1954, 2, 28, 2022, 10, 2) == (25053, 823, 68)
+    assert days_between_dates(2020, 2, 29, 2021, 3, 1) == (1,12,366)
+    assert days_between_dates(2000, 2, 29, 2023, 3, 1) == (23,276,8401)
+    assert days_between_dates(1954, 2, 28, 2022, 10, 2) == (68,823,25053)
+    assert days_between_dates(2000,12,31, 2001,1,1) == (0,0,1)
+    
     
--- src/next_year.py
+++ src/next_year.py
@@ -1,4 +1,15 @@
-def next_year(month,day):
-    if month == 12:
-        if day == 31:
+from is_leap_year import is_leap_year
+def next_year(current_month, current_day, first_month, first_day, year):
+    if first_month == 2:
+        if first_day == 29:
+            if is_leap_year(year):
+                if current_month == 2:
+                    if current_day == 29:
+                        return True
+            elif not is_leap_year(year):
+                if current_month == 2:
+                    if current_day == 28:
+                        return  True
+    if current_month == first_month:
+        if current_day == first_day:
             return True
